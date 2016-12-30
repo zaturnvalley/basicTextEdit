d66f22158ab7a7214626693b0a5012fb929e117e
zaturnvalley <zacharycmason@gmail.com>
2016-12-29 17:59:57 -0800

    more conditionals

--- code.js
+++ code.js
@@ -6,15 +6,33 @@
     console.log(div)
 
     // On Keypress, add that key to div
-    document.addEventListener('keypress',function(e){
+    document.addEventListener('keydown',function(e){
       e = e || window.event;
       var charCode = (typeof e.which == 'number') ? e.which : e.keyCode;
       if (charCode){
+        // New line if enter key is pressed
         if(e.key == 'Enter') {
         div.innerHTML = div.innerHTML + '<br>';
         return;
+        } else if (e.key == 'Meta') {
+          return;
+        } else if (e.key == 'Control') {
+          return;
+        } else if (e.key == 'Shift') {
+          return;
+        } else if (e.key == 'Alt') {
+          return;
+        } else if (e.key == 'Tab') {
+          return;
+        } else if (e.key == 'CapsLock') {
+          return;
+        } else if (e.key == 'Backspace') {
+          div.innerHTML = div.innerHTML.substring(0, div.innerHTML.length);
+          return;
         }
+        // Set div to what's already there plus new key
         div.innerHTML = div.innerHTML + e.key;
+        // Set localStorage to what's in the div
         setText(div.innerHTML)
       }
     });
