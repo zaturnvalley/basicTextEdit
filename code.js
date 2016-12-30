d8027e194413cead6988cb4fd0ea84c4679ab0a8
zaturnvalley <zacharycmason@gmail.com>
2016-12-29 16:57:29 -0800

    first commit

---
+++ code.js
@@ -0,0 +1,20 @@
+  window.onload = function(){
+    // Grab Div
+    var div = document.getElementById('myDiv');
+    console.log(div)
+
+    // On Keypress, add that key to div
+    document.addEventListener('keypress',function(e){
+      e = e || window.event;
+      var charCode = (typeof e.which == 'number') ? e.which : e.keyCode;
+      if (charCode){
+        div.innerHTML = div.innerHTML + e.key;
+      }
+    });
+
+    // Delete All text in div
+    var button = document.getElementById("button");
+    button.addEventListener('click', function() {
+      div.innerHTML = '';
+    });
+}
